0fad42efc0be73381d3e148c6d49d482e8df9157
adimass <adimaskusum03@gmail.com>
2022-07-05 03:02:07 +0700

    foward chaining applied

--- blueprint_self_diagnose.py
+++ blueprint_self_diagnose.py
@@ -2,71 +2,169 @@ from flask import Blueprint, render_template, redirect, url_for, send_file,sessi
 import requests
 import json
 import database as db
-
 bp_self_diagnose = Blueprint('bp_self_diagnose', __name__)
 
 @bp_self_diagnose.route('/self_diagnose')
 def self_diagnose():
-
     if "google_login" not in session:
         restricted_message = ""
         return render_template('login.html')
 
+    if "true" in session :
+        p_true = session['true']
+    if "false" in session :
+        f_true = session['false']
 
-    if "flag" not in session:
+    if "gejalaId" not in session:
         query ='''
         select *
         from gejala
         where gejalaId = 'G1'
         '''
+        rule = '''
+        select *
+        from relasi
+        '''
+        df = db.df_query(rule)
+        b = df.set_index('penyakitId').groupby(level=0).agg(list)
+        b['penyakitId'] = b.index
+        data = json.loads(b.to_json(orient='records'))
+        session['flag'] = data
 
     else:
-        query ='''
-        select *
-        from gejala
-        where gejalaId = 'G1'
-        '''
-    gejala = db.df_query(query)
+        penyakit = []
+        flag = session['flag']
 
+        for i in flag:
+            penyakit.append(i['penyakitId'])
+
+
+        sakit = str(penyakit)[1:-1]
+        gejala = p_true + f_true
+        gejala = str(gejala)[1:-1]
+
+        print("=========================")
+        print(penyakit)
+        print(gejala)
+        print("=========================")
+            
+        query ="""
+            
+            with data as (
+            select *
+            from relasi
+            where penyakitId in (%s)
+            ),data2 as (
+            select DISTINCT(gejalaid)
+            from data)
+            select g.gejalaid,nama,pertanyaan
+            from data2 d left join gejala g on d.gejalaid = g.gejalaid
+            where g.gejalaId not in (%s)
+            
+            """%(str(sakit),str(gejala))
+
+        print(query)
+
+
+        # if len(p_true) > 0 and len(f_true) == 0:
+        #     query ='''
+        #         select *
+        #         from gejala
+        #         where gejalaId = 'G2'
+        #         '''
+        # elif len(f_true) > 0 and len(p_true) == 0:
+        #     query ='''
+        #         select *
+        #         from gejala
+        #         where gejalaId = 'G3'
+        #         '''
+        # else :
+        #     query ='''
+        #         select *
+        #         from gejala
+        #         where gejalaId = 'G4'
+        #         '''
+    
+    gejala = db.df_query(query)
+    if len(gejala) == 0:
+        return redirect(url_for('bp_self_diagnose.result_diagnose'))
     gejalaId = gejala.iloc[0]['gejalaId']
     pertanyaan = gejala.iloc[0]['pertanyaan']
-
-    print(pertanyaan)
-    print(gejala)
 
     return render_template('content_user_self_diagnose.html',pertanyaan = pertanyaan, gejalaId = gejalaId)
 
 
 @bp_self_diagnose.route('/answer_diagnose', methods=['POST'])
 def answer_diagnose():
-
     jawaban =  request.form.get('pilihan')
     gejalaId = request.args.get("gejalaId")
+    flag = session['flag']
+    print(flag)
     print(gejalaId)
-    print(jawaban)
+    tag = []
 
-    if 'gejalaId' not in session:
-        session['gejalaId'] = []
-    # if 'flag'
+    if len(flag) != 1:
 
+        if 'gejalaId' not in session:
+            session['gejalaId'] = []
+            session['true'] = []
+            session['false'] = []
 
-    if jawaban == 'ya':
-        gejala_list = session['gejalaId']
-        gejala_list.append(gejalaId)
-        session['gejalaId'] = gejala_list
+        if jawaban == 'ya':
+            gejala_true = session['true']
+            gejala_true.append(gejalaId)
+            session['true'] = gejala_true
+            for i in flag:
+                if str(gejalaId) in i['gejalaId']:
+                    tag.append(i)
+            if len(tag) != 0:
+                session['flag'] = tag
 
+        if jawaban == 'tidak':
+            gejala_false = session['false']
+            gejala_false.append(gejalaId)
+            session['false'] = gejala_false
+            for i in flag:
+                if str(gejalaId) not in i['gejalaId']:
+                    tag.append(i)
+            if len(tag) != 0:
+                session['flag'] = tag
 
+    elif len(flag) == 1:
+        if jawaban == 'ya':
+            gejala_true = session['true']
+            gejala_true.append(gejalaId)
+            session['true'] = gejala_true
+        if jawaban == 'tidak':
+            gejala_false = session['false']
+            gejala_false.append(gejalaId)
+            session['false'] = gejala_false
+            
+
+    # if len(session['flag']) == 1 or len(session['flag']) == 0:
+    #     return 'dapet'
+    
+    print(len(session['flag']))
+
+    # if len(session['flag']) == 1:
+
+    #    return redirect(url_for('bp_self_diagnose.result_diagnose'))
+
+    print(session['flag'])
     return redirect(url_for('bp_self_diagnose.self_diagnose'))
+   
 
 @bp_self_diagnose.route('/result_diagnose', methods=['POST','GET'])
 def result_diagnose():
 
+
+    print(session['flag'])
+    print(session['true'])
+    print(session['false'])
     if 'gejalaId' in session:
         session.pop('gejalaId')
+        session.pop('true')
+        session.pop('false')
 
-    return "oke"
 
-    # if jawaban == 'ya':
-    #     return redirect(url_for('bp_self_diagnose.self_diagnose'))
-    # else:
-    #     return 'oke'
+    return 'oke'
--- fc.py
+++ fc.py
@@ -1,63 +1,63 @@
 import database as db
 import json
 
-query2 ='''
-select *
-from gejala
-'''
+# query2 ='''
+# select *
+# from gejala
+# '''
 
-df2 = db.df_query(query2)
+# df2 = db.df_query(query2)
 
-arr = []
-flag_q = []
-flag = True
-while flag is True:
-    penyakit = []
-    query = ''
-    query22 = ''
-    for i,row in df2.iterrows():
-        print(row['nama'])
-        jawab = input()
-        if jawab is 'y':
-            arr.append(row['gejalaId'])
+# arr = []
+# flag_q = []
+# flag = True
+# while flag is True:
+#     penyakit = []
+#     query = ''
+#     query22 = ''
+#     for i,row in df2.iterrows():
+#         print(row['nama'])
+#         jawab = input()
+#         if jawab is 'y':
+#             arr.append(row['gejalaId'])
 
-        flag_q.append(row['gejalaId'])
-        gejala = str(arr)[1:-1]
-        skip = str(flag_q)[1:-1]
-        query3 = """
-            with data as (
-                select *
-                from relasi
-            ) , data2 as (
-                select DISTINCT(penyakitId)
-                from data
-                where gejalaId in (%s)
-            ),data3 as (
-                select DISTINCT(gejalaId)
-                from data join data2 on data.penyakitId = data2.penyakitId
-                where gejalaId not in (%s)
-            )
-                select g.gejalaId,nama
-                from gejala g join data3 d on g.gejalaId = d.gejalaId
-            """%(gejala,skip)
+#         flag_q.append(row['gejalaId'])
+#         gejala = str(arr)[1:-1]
+#         skip = str(flag_q)[1:-1]
+#         query3 = """
+#             with data as (
+#                 select *
+#                 from relasi
+#             ) , data2 as (
+#                 select DISTINCT(penyakitId)
+#                 from data
+#                 where gejalaId in (%s)
+#             ),data3 as (
+#                 select DISTINCT(gejalaId)
+#                 from data join data2 on data.penyakitId = data2.penyakitId
+#                 where gejalaId not in (%s)
+#             )
+#                 select g.gejalaId,nama
+#                 from gejala g join data3 d on g.gejalaId = d.gejalaId
+#             """%(gejala,skip)
 
-        query44 = '''
+#         query44 = '''
         
-        with da as (
-        select *
-        from relasi
-        where gejalaId in (%s)
-        )
-        select *
-        from da
-        where gejalaId in ('%s')
+#         with da as (
+#         select *
+#         from relasi
+#         where gejalaId in (%s)
+#         )
+#         select *
+#         from da
+#         where gejalaId in ('%s')
         
-        '''%(gejala,arr[-1])
+#         '''%(gejala,arr[-1])
         
-        query22 = query44
-        query = query3
+#         query22 = query44
+#         query = query3
         
-        break
+#         break
 
 
     
@@ -96,12 +96,22 @@ df = db.df_query(rule)
 b = df.set_index('penyakitId').groupby(level=0).agg(list)
 b['penyakitId'] = b.index
 data = json.loads(b.to_json(orient='records'))
+# print(data)
+
+for i in data:
+    if 'G1' in i['gejalaId']:
+        print(i)
+    
+
+
+
+    
 
 # for i in data :
 #     if 'G1' not in i['gejalaId']:
 #         print(i['gejalaId'])
 
-print(gejala)
+# print(gejala)
 # # print(relasi)
 
 # yes = []
@@ -109,39 +119,39 @@ print(gejala)
 
 
 
-quest = []
+# quest = []
 
 
-flag = True
-while flag is True:
-    for i,row in gejala.iterrows():
-        penyakit = []
-        print(row['nama'])
-        jawab = input()
-        quest.append(row['gejalaId'])
-        if jawab is 'y':
-            for a in data :
-                if row['gejalaId'] in a['gejalaId']:
-                    penyakit.append(a['penyakitId'])
-        if jawab is 'n':
-            for a in data:
-                if row['gejalaId'] not in a['gejalaId']:
-                    penyakit.append(a['penyakitId'])  
+# flag = True
+# while flag is True:
+#     for i,row in gejala.iterrows():
+#         penyakit = []
+#         print(row['nama'])
+#         jawab = input()
+#         quest.append(row['gejalaId'])
+#         if jawab is 'y':
+#             for a in data :
+#                 if row['gejalaId'] in a['gejalaId']:
+#                     penyakit.append(a['penyakitId'])
+#         if jawab is 'n':
+#             for a in data:
+#                 if row['gejalaId'] not in a['gejalaId']:
+#                     penyakit.append(a['penyakitId'])  
 
-        # arr = []
-        # for a in range(len(data)):
-        #     if data[a]['penyakitId'] not in penyakit:
-        #         arr.append(data[a])
+#         # arr = []
+#         # for a in range(len(data)):
+#         #     if data[a]['penyakitId'] not in penyakit:
+#         #         arr.append(data[a])
 
 
-        #karena penyakit itu selali di list nol 
+#         #karena penyakit itu selali di list nol 
         
-        print(quest)
-        print(penyakit)
-        print(data)
+#         print(quest)
+#         print(penyakit)
+#         print(data)
         
-        penyakit2 = str(penyakit)[1:-1]
+#         penyakit2 = str(penyakit)[1:-1]
         
-        flag = False
+#         flag = False
 
 #     flag = False
